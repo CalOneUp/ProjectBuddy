cad97bd90ff4f44eee22f9022d235f68160d1c0a
CalOneUp <callum.saunders@oneupsales.co.uk>
2025-06-14 19:51:41 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-dark': '#0f172a', // Deep Navy Blue
+        'brand-primary': '#f97316', // Vibrant Orange
+        'brand-light': '#60a5fa', // Light Blue Accent
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
